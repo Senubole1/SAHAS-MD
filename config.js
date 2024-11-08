5fa878759153bb0307d0ca97136e625bf6d290ed
Senubole1 <bolesenu@gmail.com>
2024-11-08 20:57:04 +0530

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 
 module.exports = {
   //==========================================- MAIN - CONFIGS -==================================================================
-  SESSION_ID: process.env.SESSION_ID || "Put Session Id",
+  SESSION_ID: process.env.SESSION_ID || "kDclnKaa#yLiVcYoogeCVqd9yUn_ffq96omgfnh1vCm93ofEyZhw",
   // ADD Your Session Id 
   MONGODB: process.env.MONGODB || "mongodb+srv://kulathungaasitha319:yjHB0DvFfStNfwPS@cluster0.3oijd.mongodb.net/",
     // ADD Your MongoDB Database URL
